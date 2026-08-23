5a50446e7f7f83e0f6a3470218a25665df5e6d78
Pedro Ascencao <phascencao1@icloud.com>
2026-08-23 01:31:34 -0300

    services frontend

--- frontend/src/services/summoner.js
+++ frontend/src/services/summoner.js
@@ -5,9 +5,9 @@ const API_URL = "http://localhost:3333";
 export async function searchSummoner(riotId) {
   const [name, tag] = riotId.split("#");
 
-  const { data } = await axios.get(`${API_URL}/summoners/echo`, {
-    params: { name, tag },
-  });
+  const { data } = await axios.get(
+    `${API_URL}/riot/account/v1/accounts/by-riot-id/${name}/${tag}`,
+  );
 
   return data;
 }
